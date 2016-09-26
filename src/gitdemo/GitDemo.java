82745de51b97165d86d12e6a453d0847f63e497f
ge291147 <ge291147@C39458.ens.uqam.ca>
2016-09-26 11:01:29 -0400

    merci

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -26,6 +26,7 @@ public class GitDemo {
         System.out.println("Veuillez saisir un nom :");
         String nom = sc.nextLine();
         Hello.hello(nom);
+        System.out.println("merci");
         
     }
     
