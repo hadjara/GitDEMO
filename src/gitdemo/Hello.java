d646f34df19b8aae1b72c9dd628ccdebdd44c424
ge291147 <ge291147@C39458.ens.uqam.ca>
2016-09-26 10:19:59 -0400

    anglais

---
+++ src/gitdemo/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author ge291147
+ */
+public class Hello {
+    public static void hello(){
+        System.out.println("HELLO");
+            }
+}
